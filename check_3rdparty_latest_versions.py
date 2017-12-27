07f4c2a2e620f8b444c980ca82edae114c6706f1
Steve Robinson <ssrobins@gmail.com>
2017-12-27 13:34:06 -0800

    Make the get latest version functions as generic as possible so it's easier to add a new check

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -26,26 +26,24 @@ thirdparty_version = {
 
 
 def get_latest_version_cmake():
-    cmake_page = 'https://cmake.org/download/'
-    page = urlopen(cmake_page)
+    page = urlopen('https://cmake.org/download/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    cmake_items = soup.find('h3').text.strip().split()
+    version_items = soup.find('h3').text.strip().split()
     
-    cmake_text = cmake_items[2].strip('()')
+    version_text = version_items[2].strip('()')
     
-    return cmake_text
+    return version_text
 
 
 
 def get_latest_version_freetype():
-    freetype_page = 'https://sourceforge.net/projects/freetype/files/freetype2/'
-    page = urlopen(freetype_page)
+    page = urlopen('https://sourceforge.net/projects/freetype/files/freetype2/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    freetype_text = soup.find('a', attrs={'href': lambda L: L and L.startswith('/projects/freetype/files/freetype2/')}).text.strip()
+    version_text = soup.find('a', attrs={'href': lambda L: L and L.startswith('/projects/freetype/files/freetype2/')}).text.strip()
 
-    return str(freetype_text)
+    return str(version_text)
 
 
 
@@ -54,172 +52,150 @@ def get_latest_version_git():
     page = urlopen(req).read()
     soup = BeautifulSoup(page, 'html.parser')
 
-    git_text = soup.find('span', attrs={'class': 'version'}).text.strip()
+    version_text = soup.find('span', attrs={'class': 'version'}).text.strip()
 
-    return str(git_text)
+    return str(version_text)
 
 
 
 def get_latest_version_glew():
-    glew_page = 'http://glew.sourceforge.net/'
-    page = urlopen(glew_page)
+    page = urlopen('http://glew.sourceforge.net/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    glew_text = soup.find('a', attrs={'href': lambda L: L and L.startswith('https://sourceforge.net/projects/glew/files/glew/')}).text
+    version_text = soup.find('a', attrs={'href': lambda L: L and L.startswith('https://sourceforge.net/projects/glew/files/glew/')}).text
 
-    return str(glew_text)
+    return str(version_text)
 
 
 
 def get_latest_version_googletest():
-    googletest_page = 'https://github.com/google/googletest/releases'
-    page = urlopen(googletest_page)
+    page = urlopen('https://github.com/google/googletest/releases')
     soup = BeautifulSoup(page, 'html.parser')
 
-    googletest_text_raw = soup.find('span', attrs={'class': 'tag-name'}).text.strip()
+    version_text_raw = soup.find('span', attrs={'class': 'tag-name'}).text.strip()
 
-    googletest_text = googletest_text_raw.split('-')
+    version_text = version_text_raw.split('-')
 
-    return googletest_text[1]
+    return version_text[1]
 
 
 
 def get_latest_version_libpng():
-    libpng_page = 'http://www.libpng.org/pub/png/libpng.html'
-    page = urlopen(libpng_page)
+    page = urlopen('http://www.libpng.org/pub/png/libpng.html')
     soup = BeautifulSoup(page, 'html.parser')
 
-    libpng_text = soup.find('font', attrs={'size': '+1'}).text.strip()
+    version_text = soup.find('font', attrs={'size': '+1'}).text.strip()
     
-    return libpng_text
+    return version_text
 
 
 
 def get_latest_version_NotepadPlusPlus():
-    NotepadPlusPlus_page = 'https://notepad-plus-plus.org/download'
-    page = urlopen(NotepadPlusPlus_page)
+    page = urlopen('https://notepad-plus-plus.org/download')
     soup = BeautifulSoup(page, 'html.parser')
 
-    NotepadPlusPlus_items = soup.find('h1').text.strip().split()
+    version_items = soup.find('h1').text.strip().split()
     
-    return NotepadPlusPlus_items[-1]
+    return version_items[-1]
 
 
 
 def get_latest_version_python():
-    python_page = 'https://www.python.org/'
-    page = urlopen(python_page)
+    page = urlopen('https://www.python.org/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    python_text_items = soup.find('a', attrs={'href': lambda L: L and L.startswith('/downloads/release/python-')}).text.strip().split()
+    version_items = soup.find('a', attrs={'href': lambda L: L and L.startswith('/downloads/release/python-')}).text.strip().split()
 
-    return python_text_items[1]
+    return version_items[1]
 
 
 
 def get_latest_version_SDL2():
-    sdl2_page = 'https://www.libsdl.org/download-2.0.php'
-    page = urlopen(sdl2_page)
+    page = urlopen('https://www.libsdl.org/download-2.0.php')
     soup = BeautifulSoup(page, 'html.parser')
 
-    header_items = soup.find('h1').text.strip().split()
-
-    header_items_count = len(header_items)
-    header_items_expected_count = 4
-    if(header_items_count == header_items_expected_count):
-        if(header_items[0] == 'SDL' and header_items[1] == 'version'):
-            sdl2_version = header_items[2]
-    else:
-        print('Header count is ' + str(header_items_count) + ' but should be ' + str(header_items_expected_count))
-        exit(1)
+    version_items = soup.find('h1').text.strip().split()
         
-    return sdl2_version
+    return version_items[2]
 
 
 
 def get_latest_version_SDL2_image():
-    sdl2_image_page = 'https://www.libsdl.org/projects/SDL_image/'
-    page = urlopen(sdl2_image_page)
+    page = urlopen('https://www.libsdl.org/projects/SDL_image/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    sld2_image_text_raw = soup.find('a', attrs={'href': lambda L: L and L.startswith('release/SDL2_image-')}).text.strip()
+    version_text_raw = soup.find('a', attrs={'href': lambda L: L and L.startswith('release/SDL2_image-')}).text.strip()
 
-    sld2_image_items = re.findall(r"[\w']+", sld2_image_text_raw)
-    sld2_image_text = sld2_image_items[1] + '.' + sld2_image_items[2] + '.' + sld2_image_items[3]
+    version_items = re.findall(r"[\w']+", version_text_raw)
+    version_text = version_items[1] + '.' + version_items[2] + '.' + version_items[3]
 
-    return sld2_image_text
+    return version_text
 
 
 
 def get_latest_version_SDL2_mixer():
-    sdl2_mixer_page = 'https://www.libsdl.org/projects/SDL_mixer/'
-    page = urlopen(sdl2_mixer_page)
+    page = urlopen('https://www.libsdl.org/projects/SDL_mixer/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    sld2_mixer_text_raw = soup.find('a', attrs={'href': lambda L: L and L.startswith('release/SDL2_mixer-')}).text.strip()
+    version_text_raw = soup.find('a', attrs={'href': lambda L: L and L.startswith('release/SDL2_mixer-')}).text.strip()
 
-    sld2_mixer_items = re.findall(r"[\w']+", sld2_mixer_text_raw)
-    sld2_mixer_text = sld2_mixer_items[1] + '.' + sld2_mixer_items[2] + '.' + sld2_mixer_items[3]
+    version_items = re.findall(r"[\w']+", version_text_raw)
+    version_text = version_items[1] + '.' + version_items[2] + '.' + version_items[3]
 
-    return sld2_mixer_text
+    return version_text
 
 
 
 def get_latest_version_SDL2_ttf():
-    sdl2_ttf_page = 'https://www.libsdl.org/projects/SDL_ttf/'
-    page = urlopen(sdl2_ttf_page)
+    page = urlopen('https://www.libsdl.org/projects/SDL_ttf/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    sld2_ttf_text_raw = soup.find('a', attrs={'href': lambda L: L and L.startswith('release/SDL2_ttf-')}).text.strip()
+    version_text_raw = soup.find('a', attrs={'href': lambda L: L and L.startswith('release/SDL2_ttf-')}).text.strip()
 
-    sld2_ttf_items = re.findall(r"[\w']+", sld2_ttf_text_raw)
-    sld2_ttf_text = sld2_ttf_items[1] + '.' + sld2_ttf_items[2] + '.' + sld2_ttf_items[3]
+    version_items = re.findall(r"[\w']+", version_text_raw)
+    version_text = version_items[1] + '.' + version_items[2] + '.' + version_items[3]
 
-    return sld2_ttf_text
+    return version_text
 
 
 
 def get_latest_version_TortoiseGit():
-    TortoiseGit_page = 'https://tortoisegit.org/download/'
-    page = urlopen(TortoiseGit_page)
+    page = urlopen('https://tortoisegit.org/download/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    TortoiseGit_items = soup.find('strong').text.strip().split()
+    version_items = soup.find('strong').text.strip().split()
     
-    return TortoiseGit_items[-1]
+    return version_items[-1]
 
 
 
 def get_latest_version_VisualStudio():
-    VisualStudio_page = 'https://www.visualstudio.com/en-us/news/releasenotes/vs2017-relnotes'
-    page = urlopen(VisualStudio_page)
+    page = urlopen('https://www.visualstudio.com/en-us/news/releasenotes/vs2017-relnotes')
     soup = BeautifulSoup(page, 'html.parser')
 
-    VisualStudio_text_raw = soup.find('h2', attrs={'id': re.compile('^[0-9]+.[0-9]+.[0-9]$')}).text.strip().split()
+    version_items = soup.find('h2', attrs={'id': re.compile('^[0-9]+.[0-9]+.[0-9]$')}).text.strip().split()
     
-    return VisualStudio_text_raw[-1]
+    return version_items[-1]
 
 
 
 def get_latest_version_WinSCP():
-    WinSCP_page = 'https://winscp.net/eng/download.php'
-    page = urlopen(WinSCP_page)
+    page = urlopen('https://winscp.net/eng/download.php')
     soup = BeautifulSoup(page, 'html.parser')
 
-    WinSCP_items = soup.find('h3').text.strip().split()
+    version_items = soup.find('h3').text.strip().split()
     
-    return WinSCP_items[-1]
+    return version_items[-1]
 
 
 
 def get_latest_version_zlib():
-    zlib_page = 'https://zlib.net/'
-    page = urlopen(zlib_page)
+    page = urlopen('https://zlib.net/')
     soup = BeautifulSoup(page, 'html.parser')
 
-    zlib_text_items = soup.find('font', attrs={'size': '+2'}).text.strip().split()
+    version_items = soup.find('font', attrs={'size': '+2'}).text.strip().split()
     
-    return zlib_text_items[1]
+    return version_items[1]
 
 
 
