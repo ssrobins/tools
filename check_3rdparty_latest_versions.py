c42a46f94d4a18464700c6ffb4d3c702590b395d
Steve Robinson <ssrobins@gmail.com>
2018-02-20 22:17:15 -0800

    Upgrade Visual Studio and WinSCP

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -25,8 +25,8 @@ class VersionCheck:
             'SDL2_ttf':        {'installed': '2.0.14'},
             'SFML':            {'installed': '2.4.2'},
             'TortoiseGit':     {'installed': '2.6.0'},
-            'VisualStudio':    {'installed': '15.5.6'},
-            'WinSCP':          {'installed': '5.11.3'},
+            'VisualStudio':    {'installed': '15.5.7'},
+            'WinSCP':          {'installed': '5.13'},
             'zlib':            {'installed': '1.2.11'},
         }
         
