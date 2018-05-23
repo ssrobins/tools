39907286ee9d4d62144a90dacb3c178aa3e217d2
Steve Robinson <ssrobins@gmail.com>
2018-05-23 09:02:19 -0700

    Upgrade git on Mac

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -13,7 +13,7 @@ class VersionCheck:
             'AndroidStudio':   {'installed': '3.1.2'},
             'cmake':           {'installed': '3.11.2'},
             'freetype':        {'installed': '2.9.1'},
-            'git_mac':         {'installed': '2.16.3'},
+            'git_mac':         {'installed': '2.17.0'},
             'git_win':         {'installed': '2.17.0'},
             'glew':            {'installed': '2.1.0'},
             'googletest':      {'installed': '1.8.0'},
