350eb7cc0e278159b288a5778570f5d6cdc9ed30
Steve Robinson <ssrobins@gmail.com>
2018-10-05 16:33:41 -0700

    Upgrade git on Windows

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -20,7 +20,7 @@ class VersionCheck:
             'GIMP_mac':        {'installed': '2.10.6'},
             'GIMP_win':        {'installed': '2.10.6'},
             'git_mac':         {'installed': '2.19.0'},
-            'git_win':         {'installed': '2.19.0'},
+            'git_win':         {'installed': '2.19.1'},
             'GitLabRunner':    {'installed': '11.3.0'},
             'glew':            {'installed': '2.1.0'},
             'googletest':      {'installed': '1.8.1'},
