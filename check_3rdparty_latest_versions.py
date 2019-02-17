942c38355029223fe61f63523be7d6f7d92bd47d
Steve Robinson <ssrobins@gmail.com>
2019-02-16 19:00:18 -0800

    Note problem in latest CMake release candidate

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -14,7 +14,7 @@ class VersionCheck:
             'AndroidStudio':   {'installed': '3.3.1'},
             'bzip2':           {'installed': 'Find the latest version on SourceForge.'},
             'bzip2new':        {'installed': '1.0.6'},
-            'cmake':           {'installed': '3.14.0-rc2'},
+            'cmake':           {'installed': '3.14.0-rc2'}, # Stuck at 3.14.0 RC1: https://gitlab.kitware.com/cmake/cmake/issues/18936
             'conan':           {'installed': '1.12.2'},
             'freetype':        {'installed': '2.9.1'},
             'gcc':             {'installed': '8.2'},
