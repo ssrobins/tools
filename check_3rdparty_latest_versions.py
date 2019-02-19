cff7e0fc426a0f2aad85253fdf59ad14a2acd2ed
Steve Robinson <ssrobins@gmail.com>
2019-02-18 18:34:57 -0800

    Upgrade Conan

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -15,7 +15,7 @@ class VersionCheck:
             'bzip2':           {'installed': 'Find the latest version on SourceForge.'},
             'bzip2new':        {'installed': '1.0.6'},
             'cmake':           {'installed': '3.14.0-rc2'}, # Stuck at 3.14.0 RC1: https://gitlab.kitware.com/cmake/cmake/issues/18936
-            'conan':           {'installed': '1.12.2'},
+            'conan':           {'installed': '1.12.3'},
             'freetype':        {'installed': '2.9.1'},
             'gcc':             {'installed': '8.2'},
             'GIMP_mac':        {'installed': '2.10.8'},
