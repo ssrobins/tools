9fba1576cd836522ac987337137e278314281bff
Steve Robinson <ssrobins@gmail.com>
2019-03-20 18:21:12 -0700

    Upgrade Gradle

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -25,7 +25,7 @@ class VersionCheck:
             'GitLabRunner':    {'installed': '11.8.0'},
             'glew':            {'installed': '2.1.0'},
             'googletest':      {'installed': '1.8.1'},
-            'Gradle':          {'installed': '5.2.1'},
+            'Gradle':          {'installed': '5.3'},
             'grepWin':         {'installed': '1.7.1'},
             'KeePass':         {'installed': '2.41'},
             'libpng':          {'installed': '1.6.36'},
