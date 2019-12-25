c3f3b170e4e9af0394e1e160c46c0ca2c99ef331
Steve Robinson <ssrobins@gmail.com>
2019-12-25 15:58:10 -0800

    Upgrade GitLabRunner

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -27,7 +27,7 @@ class VersionCheck:
             'GIMP_win':        '2.10.14',
             'git_mac':         '2.23.0',
             'git_win':         '2.24.1',
-            'GitLabRunner':    '12.5.0',
+            'GitLabRunner':    '12.6.0',
             'glew':            '2.1.0',
             'googletest':      '1.10.0',
             'Gradle':          '6.0.1',
