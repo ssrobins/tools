4ffee8d21db20d87ab949bcf80d8089c4cf7b49f
Steve Robinson <ssrobins@gmail.com>
2020-01-26 22:30:14 -0800

    Update current bug for this issue because the last one was 'resolved'

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -41,7 +41,7 @@ class VersionCheck:
             "NotepadPlusPlus": "7.8.3",
             "openjdk":         "8u212-b04",
             "python":          "3.8.1",
-            "SDL2":            "2.0.10", # Stuck at 2.0.8: https://bugzilla.libsdl.org/show_bug.cgi?id=4316
+            "SDL2":            "2.0.10", # Stuck at 2.0.8: https://bugzilla.libsdl.org/show_bug.cgi?id=4601
             "SDL2_image":      "2.0.5",
             "SDL2_mixer":      "2.0.4",
             "SDL2_ttf":        "2.0.15",
