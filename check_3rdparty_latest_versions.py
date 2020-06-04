f28a299ac13d19f81038586411a1f76af4407cac
Steve Robinson <ssrobins@gmail.com>
2020-06-03 22:13:38 -0700

    Upgrade Gradle

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -34,7 +34,7 @@ class VersionCheck:
             "GitLabRunner":    "13.0.1", # Stuck at 12.5.0 on Windows: https://gitlab.com/gitlab-org/gitlab-runner/issues/6202 and won't support Windows 1903 and greater: https://gitlab.com/gitlab-org/gitlab-runner/issues/4396
             "glew":            "2.1.0",
             "googletest":      "1.10.0",
-            "Gradle":          "6.4.1",
+            "Gradle":          "6.5",
             "grepWin":         "2.0.1",
             "KeePassXC":       "2.5.4",
             "libpng":          "1.6.37",
