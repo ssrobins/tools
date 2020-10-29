b126f639a820655c6b33a9640f777b47c4223fd0
Steve Robinson <ssrobins@gmail.com>
2020-10-29 01:31:36 -0700

    Upgrade CMake

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -21,7 +21,7 @@ class VersionCheck:
             "AndroidSDKTools": "6858069",
             "AndroidStudio":   "4.1",
             "bzip2":           "1.0.8",
-            "cmake":           "3.19.0-rc1",
+            "cmake":           "3.19.0-rc2",
             "conan":           "1.30.2",
             "DockerCE":        "2.4.0.0",
             "freetype":        "2.10.4",
