6a3a0b48db51fa1399c8be7677a17390fd273e25
Steve Robinson <ssrobins@gmail.com>
2021-03-06 17:01:02 -0800

    Upgrade Docker

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -23,7 +23,7 @@ class VersionCheck:
             "bzip2":           "1.0.8",
             "cmake":           "3.20.0-rc3",
             "conan":           "1.34.0",
-            "DockerCE":        "3.2.0",
+            "DockerCE":        "3.2.1",
             "freetype":        "2.10.4",
             "gcc":             "9.3.0",
             "GIMP_mac":        "2.10.22",
