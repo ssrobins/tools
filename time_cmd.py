2fc79d74c21d95bc0bc5e32e1f1aac844495e07a
Steve Robinson <ssrobins@gmail.com>
2022-09-07 13:17:40 -0700

    Script to time a particular command

---
+++ time_cmd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+import subprocess
+import time
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cmd",
+        help="Command you want to time", required=True)
+    parser.add_argument("--dir",
+        help="Current working directory for command", required=True)
+    command_args = parser.parse_args() 
+
+    start_time = time.time()
+    subprocess.run(command_args.cmd, cwd=command_args.dir, shell=True, check=True)
+    end_time = time.time()
+
+    print(f"duration of the command '{command_args.cmd}' in hours:minutes:seconds")
+    print(str(datetime.timedelta(seconds=end_time - start_time)))
+
+
+if __name__ == "__main__":
+    main()
