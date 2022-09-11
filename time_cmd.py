1541102e1161ae6e0aa7f4e9e8cc65c0cf760db8
Steve Robinson <ssrobins@gmail.com>
2022-09-11 00:13:48 -0700

    Fix or disable pylint errors

--- time_cmd.py
+++ time_cmd.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
+"""Tool to time a command"""
+
 import argparse
 import datetime
 import subprocess
 import time
 
 def main():
+    """
+    Time running the specified command in the specified directory
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--cmd",
         help="Command you want to time", required=True)
     parser.add_argument("--dir",
         help="Current working directory for command", required=True)
-    command_args = parser.parse_args() 
+    command_args = parser.parse_args()
 
     start_time = time.time()
     subprocess.run(command_args.cmd, cwd=command_args.dir, shell=True, check=True)
