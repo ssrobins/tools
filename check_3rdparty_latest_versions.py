edb937d8fdb631025614430f7e3970b69b4e555f
Steve Robinson <ssrobins@gmail.com>
2022-10-02 12:45:45 -0700

    Upgrade OBS

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -41,7 +41,7 @@ class VersionCheck:
             "MuseScore":       "3.6.2",
             "ninja":           "1.11.1",
             "NotepadPlusPlus": "8.4.6",
-            "OBS":             "28.0.2",
+            "OBS":             "28.0.3",
             "ogg":             "1.3.5",
             "python":          "3.10.7",
             "SDL":             "2.24.0",
