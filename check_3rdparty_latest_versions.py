82cca611d5475eb41071219c37c96aa02b9341bd
Steve Robinson <ssrobins@gmail.com>
2022-12-12 18:45:44 -0800

    Upgrade git

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -30,7 +30,7 @@ class VersionCheck:
             "conan":           "2.0.0-beta6",
             "freetype":        "2.12.1",
             "GIMP":            "2.10.32",
-            "git":             "2.38.2",
+            "git":             "2.39.0",
             "glew":            "2.2.0",
             "googletest":      "1.12.1",
             "Gradle":          "7.6",
