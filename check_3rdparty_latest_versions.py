8ea7a90fba08f6808cbabb74db5c680f26fd5d7c
Steve Robinson <ssrobins@gmail.com>
2024-02-22 16:55:40 -0800

    Upgrade VS2022

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -50,7 +50,7 @@ class VersionCheck:
             "SDL_ttf":         "2.22.0",
             "SFML":            "2.6.1",
             "vorbis":          "1.3.7",
-            "VS2022":          "17.9.0",
+            "VS2022":          "17.9.1",
             "Xcode":           "15.2",
             "zlib":            "1.3.1",
         }
