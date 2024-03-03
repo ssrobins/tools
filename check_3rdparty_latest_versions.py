ae418dea181e1c7186d353b37028642e05f49695
Steve Robinson <ssrobins@gmail.com>
2024-03-03 14:21:21 -0800

    Upgrade libpng

--- check_3rdparty_latest_versions.py
+++ check_3rdparty_latest_versions.py
@@ -37,7 +37,7 @@ class VersionCheck:
             "Gradle":          "8.6",
             "grepWin":         "2.0.15",
             "KeePassXC":       "2.7.6",
-            "libpng":          "1.6.42",
+            "libpng":          "1.6.43",
             "MuseScore":       "4.2.1",
             "ninja":           "1.11.1",
             "NotepadPlusPlus": "8.6.4",
